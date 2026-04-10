f123c3f9cfc6e6ced40b78f3840d3b875feff9f8
Krazekidd <candacehendricks1000@gmail.com>
2026-04-10 12:05:53 -0500

    fixed

---
+++ client/src/pages/Account/Account.jsx
@@ -0,0 +1,8 @@
+export default function Account() {
+  return (
+    <div>
+      <h1>Account Page</h1>
+      <p>This is the account page where users can view and manage their account details.</p>
+    </div>
+  )
+}

72dde1f8d7a99c71320c565af6c4c986ea5c0fad
Krazekidd <candacehendricks1000@gmail.com>
2026-05-07 20:39:07 -0500

    clean

--- backend/test_conn.py
+++
@@ -1,8 +0,0 @@
-import asyncio, asyncpg
-
-async def t():
-    conn = await asyncpg.connect(host='127.0.0.1', port=5440, user='postgres', password='postgres', database='chatbot_db', ssl=False)
-    print(await conn.fetchval('SELECT 1'))
-    await conn.close()
-
-asyncio.run(t())
